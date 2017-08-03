455441a0acc9063325436696c5eb89a2d2c33c32
imrebartis <imbartis@gmail.com>
2017-08-03 16:26:35 +0300

    production version done

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -14,8 +14,8 @@ class SearchBar extends Component {
     return (
         // u can render the value of the input field e.g.
         // inserting into the div: Value of the input: {this.state.term}
-      <div className="search-bar">
-        <input
+      <div className="search-bar" style={{margin: '20px', textAlign: 'center'}}>
+        <input style={{width: '75%'}}
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)} />
       </div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,41 @@
-module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var config = {
+    entry: './src/index.js',
+    output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+},
+module: {
+    rules: [
+    { test: /\.(js)$/, use: 'babel-loader' },
+    { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+    ]
+},
+devServer: {
+    historyApiFallback: true
+},
+plugins: [
+    new HtmlWebpackPlugin({
+    template: 'index.html'
+    })
+  ]
 };
+
+// set a NODE_ENV to production
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+    new webpack.DefinePlugin({
+    'process.env': {
+    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }
+}),
+// minify the code
+new webpack.optimize.UglifyJsPlugin()
+ )
+}
+
+module.exports = config;
